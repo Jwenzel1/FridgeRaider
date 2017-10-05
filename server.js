d86f8b384bb860fcf9e0d41a2fbf6f0508219b38
Joe <joewenzel45@gmail.com>
2017-10-05 16:15:16 -0400

    changes

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
@@ -25,6 +25,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
 app.use(express.static("client/build"));
+app.use(express.static("/routes"))
 // Add routes, both API and view
 app.use(routes);
 
